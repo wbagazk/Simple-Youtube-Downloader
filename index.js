76c64f7ace320e486ab5760a785bf16b441236ac
wbagazk <info.wirdiyanbagask@gmail.com>
2025-04-01 16:55:25 +0700

    ✨ Simple Website Youtube Downloader with API

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-router.get('/video/:file', async (req, res) => {
+app.get('/download/:file', async (req, res) => {
     const file = req.params.file;
     const originalUrl = `https://ytdown.siputzx.my.id/download/${file}`;
 
