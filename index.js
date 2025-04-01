a2f93ac125f6b8bee57a914d504962b99095f830
wbagazk <info.wirdiyanbagask@gmail.com>
2025-04-01 15:17:57 +0700

    ✨ Simple Website Youtube Downloader with API

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "test.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/monitor-page", (req, res) => {
